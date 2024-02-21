37e02967657f20f3360a34d843b707fc488d2cee
Varun Reddy <tvarun2014@gmail.com>
2024-02-21 19:39:05 +0530

    New

---
+++ 132_Inheritance.cpp
@@ -0,0 +1,33 @@
+// Inheritance: A class can be derived from more than one classes, which means it can inherit data and functions from multiple base classes.
+// It is possible to inherit attributes and methods from one class to another. We group the "inheritance concept" into two categories:
+
+// derived class (child) - the class that inherits from another class
+// base class (parent) - the class being inherited from
+
+// It is useful for code reusability: reuse attributes and methods of an existing class when you create a new class.
+
+
+#include<iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+  public:
+    string brand = "Ford";
+    void honk() {
+      cout << "Tuut, tuut! \n" ;
+    }
+};
+
+// Derived class
+class Car: public Vehicle {
+  public:
+    string model = "Mustang";
+};
+
+int main() {
+  Car myCar;
+  myCar.honk();
+  cout << myCar.brand + " " + myCar.model;
+  return 0;
+}
---
+++ 134_Function_Overloading.cpp
@@ -0,0 +1,33 @@
+// In C++, two functions can have the same name if the number and/or type of arguments passed is different.
+
+// These functions having the same name but different arguments are known as overloaded functions.
+
+// Program to compute absolute value
+// Works for both int and float
+
+#include <iostream>
+using namespace std;
+
+// function with float type parameter
+float absolute(float var){
+    if (var < 0.0)
+        var = -var;
+    return var;
+}
+
+// function with int type parameter
+int absolute(int var) {
+     if (var < 0)
+         var = -var;
+    return var;
+}
+
+int main() {
+    
+    // call function with int type parameter
+    cout << "Absolute value of -5 = " << absolute(-5) << endl;
+
+    // call function with float type parameter
+    cout << "Absolute value of 5.5 = " << absolute(5.5f) << endl;
+    return 0;
+}
