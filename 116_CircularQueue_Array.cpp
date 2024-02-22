5c51017ea5d58bab84e45b4c81fcf8f53b576788
Varun Reddy <tvarun2014@gmail.com>
2024-02-22 13:23:42 +0530

    New

--- 116_CircularQueue_Array.cpp
+++ 116_CircularQueue_Array.cpp
@@ -1,127 +1,65 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int cirQueue[5];
-int front = -1, rear = -1, n = 5;
-
-void insertCQ(int val)
-{
-    if ((front == 0 && rear == n - 1) || front == rear + 1)
-    {
-        cout << "Queue Overflow" << endl;
-        return;
+#define N 5
+int arr[N];
+int front = -1;
+int rear = -1;
+void enqueue(int x){
+    if((rear+1)%N == front){
+        cout<<"Overflow"<<endl;
     }
-    if (front == -1)
-    {
-        front = 0;
-        rear = 0;
+    else if(front==-1 && rear==-1){
+        front = rear = 0;
+        arr[rear] = x;
     }
-    else
-    {
-        if (rear == n - 1)
-        {
-            rear = 0;
-        }
-        else
-        {
-            rear = rear + 1;
-        }
-    }
-    cirQueue[rear] = val;
-}
-
-void deleteCQ()
-{
-    if (front == -1)
-    {
-        cout << "Queue Underflow" << endl;
-        return;
-    }
-    if (front == rear)
-    {
-        front = -1;
-        rear = -1;
-    }
-    else
-    {
-        if (front == n - 1)
-        {
-            front = 0;
-        }
-        else
-        {
-            front = front + 1;
-        }
+    else{
+        rear = (rear+1)%N;
+        arr[rear] = x;
     }
 }
-
-void display()
-{
-    int f = front;
-    int r = rear;
-
-    if (front == -1)
-    {
-        cout << "Queue is empty" << endl;
-        return;
+void dequeue(){
+    if(front==-1 && rear==-1){
+        cout<<"Undequeuerflow"<<endl;
     }
-    cout << "Queue elements are : " << endl;
-
-    if (f <= r)
-    {
-        while (f <= r)
-        {
-            cout << cirQueue[f] << " ";
-            f++;
-        }
+    else if(front==rear){
+        front = rear = -1;
     }
-    else
-    {
-        while (f <= n - 1)
-        {
-            cout << cirQueue[f] << " ";
-            f++;
-        }
-        f = 0;
-        while (f <= r)
-        {
-            cout << cirQueue[f] << " ";
-            f++;
-        }
+    else{
+        front=(front+1)%N;
     }
-    cout << endl;
 }
-
+void display(){
+    if(front==-1 && rear==-1){
+        cout<<"Undequeuerflow"<<endl;
+    }
+    else{
+        int i = front;
+        while(i!=rear){
+            cout<<arr[i]<<",";
+            i = (i+1)%N;
+        }
+        cout<<arr[i]<<endl;
+        }
+}
 int main()
 {
-    int ch, val;
-    cout << "1)Insert\n";
-    cout << "2)Delete\n";
-    cout << "3)Display\n";
-    cout << "4)Exit\n";
-    do
-    {
-        cout << "Enter choice : " << endl;
-        cin >> ch;
-        switch (ch)
-        {
-        case 1:
-            cout << "Input for insertion: " << endl;
-            cin >> val;
-            insertCQ(val);
-            break;
-        case 2:
-            deleteCQ();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            cout << "Exit\n";
-            break;
-        default:
-            cout << "Incorrect!\n";
-        }
-    } while (ch != 4);
+    enqueue(1);
+    enqueue(2);
+    enqueue(3);
+    enqueue(4);
+    enqueue(5);
+    display();
+    enqueue(6);
+    dequeue();
+    display();
+    dequeue();
+    display();
+    enqueue(6);
+    display();
+    enqueue(7);
+    display();
+    enqueue(8);
+    dequeue();
+    display();
     return 0;
 }
