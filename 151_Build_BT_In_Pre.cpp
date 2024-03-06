e545a3816feed8897029eeb46d8aeee74c792089
Varun Reddy <tvarun2014@gmail.com>
2024-03-06 20:40:42 +0530

    Build Binary Tree

---
+++ 151_Build_BT_In_Pre.cpp
@@ -0,0 +1,107 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node * left;
+        Node * right;
+        Node(int data){
+            this->val = data;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+class BinaryTree{
+    public:
+        Node * root;
+        BinaryTree(){
+            root = NULL;
+        }
+
+        Node * create(Node * root){
+             int data;
+            cin >> data;
+            if(data == -1){
+                return NULL;
+            }
+            else{
+                root = new Node(data);
+            }
+            cout << "Enter the left child of " << data << " : ";
+            root->left = create(root->left);
+            cout << "Enter the right child of " << data << " : ";
+            root->right = create(root->right);
+            return root;
+        }
+
+        int height(Node * root){
+            if(root == NULL){
+                return 0;
+            }
+            int left = height(root->left);
+            int right = height(root->right);
+            int ans = max(left,right) + 1;
+            return ans;
+        }
+
+        void printLevel(Node * root, int i){
+            if(root == NULL){
+                return;
+            }
+            else if(i == 0){
+                cout << root->val << " ";
+            }
+            else if(i>0){
+                printLevel(root->left,i-1);
+                printLevel(root->right,i-1);
+            }
+        }
+        void levelOrderDisplay(Node * root){
+            int h = height(root);
+            for(int i = 0; i<h; i++){
+                printLevel(root,i);
+                cout << endl;
+            }
+        }
+        int findPos(int inorder[], int element, int n){
+            for(int i = 0; i<n; i++){
+                if(inorder[i] == element){
+                    return i;
+                }
+            }
+            return -1;
+        }
+        Node * solve(int inorder[],int preorder[],int &index, int inorderStart, int inorderEnd, int n){
+            if(index >= n || inorderStart > inorderEnd){
+                return NULL;
+            }
+            int element = preorder[index++];
+            Node * root = new Node(element);
+            int pos = findPos(inorder,element,n);
+
+            root->left = solve(inorder,preorder,index,inorderStart,pos-1,n);
+            root->right = solve(inorder,preorder,index,pos+1,inorderEnd,n);
+            return root;    
+
+        }
+        Node * buildTree(int inorder[],int preorder[],int n){
+            int preOrderIndex = 0; // Root index
+            Node * res = solve(inorder,preorder,preOrderIndex,0,n-1,n);
+        }
+};
+
+int main()
+{
+    BinaryTree bt;
+    int inorder[] = {3,1,4,0,5,2};
+    int preorder[] = {0,1,3,4,2,5};
+    int n = 6;
+    bt.root = bt.buildTree(inorder,preorder,n);
+
+    bt.levelOrderDisplay(bt.root);
+    return 0;
+}
